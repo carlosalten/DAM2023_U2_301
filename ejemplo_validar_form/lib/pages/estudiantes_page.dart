f9e431a15335531ef2afab4476231d4a038b631f
Carlos Alten <carlos.alten@usm.cl>
2023-11-15 21:30:13 -0300

    Inicio clase login

--- ejemplo_validar_form/lib/pages/estudiantes_page.dart
+++ ejemplo_validar_form/lib/pages/estudiantes_page.dart
@@ -1,14 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ejemplo_validar_form/pages/estudiante_agregar.dart';
 import 'package:ejemplo_validar_form/services/firestore_service.dart';
+import 'package:ejemplo_validar_form/widgets/campo_estudiante.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class EstudiantesPage extends StatelessWidget {
-  // const EstudiantesPage({Key? key}) : super(key: key);
-
   final formatoFecha = DateFormat('dd-MM-yyyy');
 
   @override
@@ -16,53 +15,70 @@ class EstudiantesPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Estudiantes Firebase'),
-        leading: Icon(
-          MdiIcons.firebase,
-          color: Colors.yellow.shade700,
-        ),
+        leading: Icon(MdiIcons.firebase, color: Colors.yellow.shade700),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (context) => [PopupMenuItem(child: Text('Cerrar Sesión'), value: 'logout')],
+            onSelected: (opcion) {},
+          ),
+        ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: StreamBuilder(
-          stream: FirestoreService().estudiantes(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData || snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else {
-              return ListView.separated(
-                separatorBuilder: (context, index) => Divider(),
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  var estudiante = snapshot.data!.docs[index];
-                  return Slidable(
-                    endActionPane: ActionPane(
-                      motion: ScrollMotion(),
-                      children: [
-                        SlidableAction(
-                          backgroundColor: Colors.red,
-                          icon: MdiIcons.trashCan,
-                          label: 'Borrar',
-                          onPressed: (context) {
-                            FirestoreService().estudianteBorrar(estudiante.id);
-                          },
-                        ),
-                      ],
-                    ),
-                    child: ListTile(
-                      leading: Icon(MdiIcons.account),
-                      title: Text('${estudiante['nombre']} ${estudiante['apellido']}'),
-                      subtitle: Text(estudiante['carrera']),
-                      // subtitle: Text('Fecha de Matrícula: ' + formatoFecha.format(estudiante['fecha_matricula'].toDate())),
-                      onLongPress: () {
-                        mostrarInfoEstudiante(context, estudiante);
+      body: Column(
+        children: [
+          //EMAIL USER
+          Container(
+            padding: EdgeInsets.all(10),
+            color: Color(0xFF051E34),
+            width: double.infinity,
+            child: Text('holamundo@usm.cl', style: TextStyle(color: Colors.white)),
+          ),
+          //LISTA DE ESTUDIANTES
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: StreamBuilder(
+                stream: FirestoreService().estudiantes(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (!snapshot.hasData || snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else {
+                    return ListView.separated(
+                      separatorBuilder: (context, index) => Divider(),
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        var estudiante = snapshot.data!.docs[index];
+                        return Slidable(
+                          endActionPane: ActionPane(
+                            motion: ScrollMotion(),
+                            children: [
+                              SlidableAction(
+                                backgroundColor: Colors.red,
+                                icon: MdiIcons.trashCan,
+                                label: 'Borrar',
+                                onPressed: (context) {
+                                  FirestoreService().estudianteBorrar(estudiante.id);
+                                },
+                              ),
+                            ],
+                          ),
+                          child: ListTile(
+                            leading: Icon(MdiIcons.account),
+                            title: Text('${estudiante['nombre']} ${estudiante['apellido']}'),
+                            subtitle: Text(estudiante['carrera']),
+                            // subtitle: Text('Fecha de Matrícula: ' + formatoFecha.format(estudiante['fecha_matricula'].toDate())),
+                            onLongPress: () {
+                              mostrarInfoEstudiante(context, estudiante);
+                            },
+                          ),
+                        );
                       },
-                    ),
-                  );
+                    );
+                  }
                 },
-              );
-            }
-          },
-        ),
+              ),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -75,25 +91,39 @@ class EstudiantesPage extends StatelessWidget {
   }
 
   void mostrarInfoEstudiante(BuildContext context, estudiante) {
-    // print(estudiante['nombre']);
     showBottomSheet(
       context: context,
       builder: (context) {
         return SizedBox(
           height: 350,
           child: Container(
+            decoration: BoxDecoration(
+              color: Colors.lightBlue.shade50,
+              border: Border.all(color: Colors.blue.shade900, width: 2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.0),
+                topRight: Radius.circular(10.0),
+              ),
+            ),
             padding: EdgeInsets.all(10),
             width: double.infinity,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Nombre: ${estudiante['nombre']} ${estudiante['apellido']}'),
-                Text('Carrera: ${estudiante['carrera']}'),
-                Text('Edad:${estudiante['edad']}'),
+                Container(
+                  margin: EdgeInsets.fromLTRB(5, 0, 0, 10),
+                  child: Text('Información del Estudiante', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Color(0xFF051E34))),
+                ),
+                CampoEstudiante(dato: '${estudiante['nombre']} ${estudiante['apellido']}', icono: MdiIcons.account),
+                CampoEstudiante(dato: '${estudiante['edad']} años', icono: MdiIcons.cake),
+                CampoEstudiante(dato: '${estudiante['carrera']}', icono: MdiIcons.schoolOutline),
+                CampoEstudiante(dato: estudiante['jornada'] == 'd' ? 'Jornada Diurna' : 'Jornada Vespertina', icono: MdiIcons.timelapse),
+                CampoEstudiante(dato: formatoFecha.format(estudiante['fecha_matricula'].toDate()), icono: MdiIcons.calendarRange),
                 Spacer(),
                 Container(
                   width: double.infinity,
                   child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(backgroundColor: Colors.white),
                     child: Text('Cerrar'),
                     onPressed: () => Navigator.pop(context),
                   ),
